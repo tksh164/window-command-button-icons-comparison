97a93922b738d3cca7381652ba23988582d40c4e
Takeshi Katano <takatano@outlook.com>
2021-11-03 22:03:35 +0900

    Initial code

---
+++ src/window-command-button-icons-comparison/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace window_command_button_icons_comparison
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
